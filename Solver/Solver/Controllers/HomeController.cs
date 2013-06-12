269fe758a6dca7e78251037382f79e435c8d016b
Valentina <muas456@gmail.com>
2013-06-12 19:12:32 +0600

    first version

---
+++ Solver/Solver/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using Solver.Tools;
+
+namespace Solver.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult GetResult(string equations)
+        {
+            var equationsList = new JavaScriptSerializer().Deserialize<IEnumerable<string>>(equations).ToList();
+            RemoveWhiteSpaces(equationsList);
+            if (!equationsList.SystemIsCorrect()) return Json(new {result = false, reason = "It's not a system"});
+            if (!SystemIsSolvable(equationsList)) return Json(new { result = false, reason = "System cannot be solved" });
+            var matrix = equationsList.GetMatrix();
+            var isSolved = LinearEquationSolver.Solve(matrix);
+            if (isSolved)
+            {
+                var variables = Parser.GetAllVariables(equationsList);
+                var resultList = new List<string>();
+                for (int i = 0; i < variables.Count; i++)
+                {
+                    resultList.Add(string.Format("{0}={1}", variables.ElementAt(i),matrix[i,variables.Count]));
+                }
+                return Json(new {result = true, reason = resultList});
+            }
+            return Json(new {result = false});
+        }
+
+        public void RemoveWhiteSpaces(List<string> equations)
+        {
+            int i = 0;
+            while (i < equations.Count)
+            {
+                equations[i].Trim();
+                if (string.IsNullOrWhiteSpace(equations[i]))
+                {
+                    equations.Remove(equations[i]);
+                    continue;
+                }
+                equations[i].ToLower();
+                var index = equations[i].IndexOf(' ');
+                while (index > 0)
+                {
+                   equations[i] = equations[i].Remove(index, 1);
+                   index = equations[i].IndexOf(' ');
+                }
+                i++;
+            }
+            
+         }
+
+        public bool SystemIsSolvable(List<string> equations)
+        {
+            var variables = Parser.GetAllVariables(equations);
+            return variables.Any() && variables.Count <= equations.Count;
+        }
+
+    }
+}
---
+++ Solver/Solver/Tools/Parser.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solver.Tools
+{
+    public static class Parser
+    {
+        private static readonly char[] Signs = {'-','+','*','/','='};
+
+        public static SortedSet<char> GetAllVariables(List<string> equations)
+        {
+            var variables = new SortedSet<char>();
+             foreach (var equation in equations)
+             {
+                 foreach (var symbol in equation)
+                 {
+                     if (symbol >= 'a' && symbol <= 'z')
+                         variables.Add(symbol);
+                 }
+             }
+            return variables;
+        }
+
+        public static bool SystemIsCorrect(this List<string> equations)
+        {
+            foreach (var equation in equations)
+            {
+                foreach (var symbol in equation)
+                {
+                    if (!Signs.Any(x => x == symbol) && !(symbol >= 'a' && symbol <= 'z') && !(symbol >='0' && symbol <='9'))
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public static float[,] GetMatrix(this List<string> equations)
+        {
+            var variables = GetAllVariables(equations);
+            float[,] matrix = new float[variables.Count,variables.Count + 1];
+            var i = 0;
+            foreach (var equation in equations)
+            {
+                var equalityPosition = equation.IndexOf('=');
+                var conditionString = equation.Substring(0,equalityPosition);
+                var resultString = equation.Substring(equalityPosition + 1);
+
+                try
+                {
+                    var conditionList = ParseCondition(conditionString);
+                    var result = Convert.ToSingle(resultString);
+                    var j = 0;
+                    foreach (var variable in variables)
+                    {
+                        if (conditionList.ContainsKey(variable))
+                            matrix[i, j] = conditionList.ElementAt(conditionList.IndexOfKey(variable)).Value;
+                        else matrix[i, j] = 0;
+                        j++;
+                    }
+                    matrix[i, variables.Count] = result;
+                }
+                catch(Exception)
+                {
+                    
+                }
+                i++;
+            }
+            return matrix;
+        }
+
+        private static SortedList<char,float> ParseCondition(string conditionString)
+        {
+            var conditionList = new SortedList<char, float>();
+            var i = 0;
+            var currentCoefficient = string.Empty;
+            while (i < conditionString.Length)
+            {
+                if (conditionString[i] >= '0' && conditionString[i] <= '9' || conditionString[i] == '.' || conditionString[i] == '-')
+                {
+                    currentCoefficient += conditionString[i];
+                }
+                if (conditionString[i] >= 'a' && conditionString[i] <= 'z')
+                {
+                    float coefficient;
+                    if (currentCoefficient == "-")
+                        coefficient = -1;
+                    else if (currentCoefficient == string.Empty)
+                        coefficient = 1;
+                    else coefficient = Convert.ToSingle(currentCoefficient);
+                    conditionList.Add(conditionString[i],coefficient);
+                    currentCoefficient = string.Empty;
+                }
+                i++;
+            }
+            return conditionList;
+        }
+ 
+
+    }
+}
