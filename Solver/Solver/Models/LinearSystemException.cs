f2e7f9e9e3e0dd583f1d3b1110608cc08b25e48d
Valentina <muas456@gmail.com>
2013-06-15 14:07:45 +0600

    add new classes for exception and return value

---
+++ Solver/Solver/Models/LinearSystemException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solver.Models
+{
+    public class LinearSystemException : Exception
+    {
+         public LinearSystemException(string message) : base(message)
+         {
+            
+         }
+    }
+}
---
+++ Solver/Solver/Models/Reason.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Solver.Models
+{
+    public class Reason
+    {
+        public bool isMathematical;
+        public string reasonText;
+    }
+}
